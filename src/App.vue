0604ecbd6e3752737b6bf05eddb18afaba89b191
darkhacker1906 <singhabhinandan634@gmail.com>
2024-03-28 18:14:35 +0530

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <home-page></home-page>
+</template>
+
+<script>
+import HomePage from './pages/HomePage.vue'
+export default {
+  components: { HomePage },
+name:'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>This is my home page</h1>
+    <v-btn>Hello</v-btn>
+</template>
+
+<script>
+export default {
+name:"HomePage"
+}
+</script>
+
+<style scoped>
+h1{
+    text-align: center;
+}
+</style>
